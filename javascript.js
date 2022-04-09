3bdbb33aaf767f2ef88d2df8c1c5c0360274583f
Gage <yaujin@protonmail.com>
2022-04-09 17:46:08 -0500

    made each round console logged and return the result as a variable. Also formatted code to be more readable

--- javascript.js
+++ javascript.js
@@ -9,38 +9,57 @@ function computerPlay() {
   
 }
 
-const playerSelection = window.prompt('Choose your weapon');
+
 
 const computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection.toLowerCase() === 'rock')  {
     if (computerSelection === 'rock') {
-      return 'Tie try again!';
+        let tie = console.log('Tie try again!');
+        let result = tie;
+        return result;
     } else if (computerSelection === 'paper') {
-      return 'Computer wins paper beats rock!';
+        let lost = console.log('Computer wins paper beats rock!');
+        let result = lost;
+        return result;
     } else if (computerSelection === 'scissors') {
-      return 'You win rock beats scissors';
+        let won = console.log('You win rock beats scissors!');
+        let result = won;
+        return result;
     }
     
 } 
   else if (playerSelection.toLowerCase() === 'paper') {
     if (computerSelection === 'rock') {
-      return 'You win paper beats rock!';
+      let won = console.log('You win paper beats rock!');
+      let result = won;
+      return result;
     } else if (computerSelection === 'paper') {
-      return 'Tie try again!';
+        let tie = console.log('Tie try again!');
+        let result = tie;
+        return result;
     } else if (computerSelection === 'scissors') {
-      return 'Computer wins scissors beats rock';
+        let lost = console.log('Computer wins scissors beats rock!');
+        let result = lost;
+        return result;
     }
     
 }
   else if (playerSelection.toLowerCase() === 'scissors') { 
     if (computerSelection === 'rock') {
-      return 'Computer wins rock beats scissors!';
+      let lost = console.log('Computer wins rock beats scissors!');
+      let result = lost;
+      return result;
     } else if (computerSelection === 'paper') {
-      return 'You win scissors beats paper';
+        let win = console.log('You win scissors beats paper');
+        let result = win;
+        return result;
     } else if (computerSelection === 'scissors)') {
-      return 'Tie try again!';
+        let tie = console.log('Tie try again!');
+        let result = tie;
+        return result;
     }
 }
-}
+
+game();
