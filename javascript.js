8669b3eb669de39023bc5aed8834753f902ddbf5
Gage <yaujin@protonmail.com>
2022-04-09 14:19:48 -0500

    made playRound function to calculate who wins

--- javascript.js
+++ javascript.js
@@ -12,3 +12,12 @@ function computerPlay() {
 const playerSelection = window.prompt('Choose your weapon');
 
 const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Rock') {
+    console.log('Tie try again!');
+} else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Paper') {
+    console.log('Computer wins!');
+} else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'Scissors') { console.log('Player wins!');
+}
+}
