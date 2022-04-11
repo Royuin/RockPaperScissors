68fc74cec18b493cd4c78b052358c700587b48eb
Gage <yaujin@protonmail.com>
2022-04-11 15:39:07 -0500

    formatted code to be more readable

--- javascript.js
+++ javascript.js
@@ -14,68 +14,61 @@ function computerPlay() {
 const computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() === 'rock')  {
-    if (computerSelection === 'rock') {
-        let tie = console.log('Tie try again!');
-        let result = tie;
-        return result;
-    } else if (computerSelection === 'paper') {
-        let lost = console.log('Computer wins paper beats rock!');
-        let result = lost;
-        return result;
-    } else if (computerSelection === 'scissors') {
-        let won = console.log('You win rock beats scissors!');
+    if (playerSelection.toLowerCase() === 'rock')  {
+      if (computerSelection === 'rock') {
+          let tie = console.log('Tie try again!');
+          let result = tie;
+          return result;
+      } else if (computerSelection === 'paper') {
+          let lost = console.log('Computer wins paper beats rock!');
+          let result = lost;
+          return result;
+      } else if (computerSelection === 'scissors') {
+          let won = console.log('You win rock beats scissors!');
+          let result = won;
+          return result;
+      }
+    
+  } 
+    else if (playerSelection.toLowerCase() === 'paper') {
+      if (computerSelection === 'rock') {
+        let won = console.log('You win paper beats rock!');
         let result = won;
         return result;
-    }
+      } else if (computerSelection === 'paper') {
+          let tie = console.log('Tie try again!');
+          let result = tie;
+          return result;
+      } else if (computerSelection === 'scissors') {
+          let lost = console.log('Computer wins scissors beats rock!');
+          let result = lost;
+          return result;
+      }
     
-} 
-  else if (playerSelection.toLowerCase() === 'paper') {
-    if (computerSelection === 'rock') {
-      let won = console.log('You win paper beats rock!');
-      let result = won;
-      return result;
-    } else if (computerSelection === 'paper') {
-        let tie = console.log('Tie try again!');
-        let result = tie;
-        return result;
-    } else if (computerSelection === 'scissors') {
-        let lost = console.log('Computer wins scissors beats rock!');
+  }
+    else if (playerSelection.toLowerCase() === 'scissors') { 
+      if (computerSelection === 'rock') {
+        let lost = console.log('Computer wins rock beats scissors!');
         let result = lost;
         return result;
-    }
-    
-}
-  else if (playerSelection.toLowerCase() === 'scissors') { 
-    if (computerSelection === 'rock') {
-      let lost = console.log('Computer wins rock beats scissors!');
-      let result = lost;
-      return result;
-    } else if (computerSelection === 'paper') {
-        let win = console.log('You win scissors beats paper');
-        let result = win;
-        return result;
-    } else if (computerSelection === 'scissors)') {
-        let tie = console.log('Tie try again!');
-        let result = tie;
-        return result;
-    }
-}
-
+      } else if (computerSelection === 'paper') {
+          let win = console.log('You win scissors beats paper');
+          let result = win;
+          return result;
+      } else if (computerSelection === 'scissors)') {
+          let tie = console.log('Tie try again!');
+          let result = tie;
+          return result;
+      }
+  } 
+}   
 
 function game() {
   
   for (let i = 0; i < 5; i++) {
-    let playerSelection = window.prompt('Choose your weapon');
-  
-  playRound(playerSelection, computerSelection);
-
-  
-  
+    let playerSelection = window.prompt('WEAPON?');
+    playRound(playerSelection, computerSelection)
   }
-  
-
-  
 }
 
 game();
