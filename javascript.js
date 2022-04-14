cd47a5fb001c4d39e433ae94683b6ecb399b1a6f
Gage <yaujin@protonmail.com>
2022-04-13 20:58:27 -0500

    added player and computer score. Also added if statement to add scores if round is win or lose

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let computerScore = 0; 
+let playerScore = 0;
+
 let rpsArray = ['rock', 'paper', 'scissors'];
 
 function computerPlay() {
@@ -14,16 +17,21 @@ let computerSelection = computerPlay();
 
 
 function playRound(playerSelection, computerSelection) {
+  
   computerSelection = computerPlay();
+  
   if (playerSelection.toLowerCase() === 'rock')  {
       if (computerSelection === 'rock') {
            tie = console.log('Tie try again!');
+          result = tie;
           return tie;
       } else if (computerSelection === 'paper') {
            lose = console.log('Computer wins paper beats rock!');
-          return lose;
+          result = lose;
+          return result;
       } else if (computerSelection === 'scissors') {
            win = console.log('You win rock beats scissors!');
+          result = win;
           return win;
       }
     
@@ -31,12 +39,15 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection.toLowerCase() === 'paper') {
       if (computerSelection === 'rock') {
          win = console.log('You win paper beats rock!');
+        result = win;
         return win;
       } else if (computerSelection === 'paper') {
            tie = console.log('Tie try again!');
-          return tie;
+            result = tie          
+           return result;
       } else if (computerSelection === 'scissors') {
            lose = console.log('Computer wins scissors beats rock!');
+          result = lose;
           return lose;
       }
     
@@ -44,19 +55,28 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection.toLowerCase() === 'scissors') { 
       if (computerSelection === 'rock') {
          lose = console.log('Computer wins rock beats scissors!');
-        return lose;
+        result = lose;
+        return lose
       } else if (computerSelection === 'paper') {
            win = console.log('You win scissors beats paper');
+          result = win;
           return win;
       } else if (computerSelection === 'scissors)') {
            tie = console.log('Tie try again!')
+          result = tie;
           return tie;
       }
   } 
-    
+  if (result == win) {
+    playerScore++;
+  } else if (result == lose) {
+    computerScore++;
+  }
 }   
 
 function game() {
+  computerScore = 0;
+  playerScore = 0;
   
   for (let i = 0; i < 5; i++) {
     let playerSelection = window.prompt('Choose your weapon rock, paper, or scissors?');
